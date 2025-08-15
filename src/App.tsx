b0bdb6fb0ea947872f9ad74d3d66b61d14d1291f
ikurasusshi <kaikai180208@gmail.com>
2025-08-15 17:01:03 +0900

    setup of font

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import './App.css';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Report from './pages/Report';
-import NoMatch from './pages/NoMatch';
-import AppLayout from './components/layout/AppLayout';
+import React from "react";
+import "./App.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Report from "./pages/Report";
+import NoMatch from "./pages/NoMatch";
+import AppLayout from "./components/layout/AppLayout";
+import { theme } from "./theme/theme";
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Router>
         <Routes>
-          <Route path='/' element={<AppLayout />}>
-            <Route index element={<Home />}/>
-            <Route path='/report' element={<Report />}/>
-            <Route path='*' element={<NoMatch />}/>
+          <Route path="/" element={<AppLayout />}>
+            <Route index element={<Home />} />
+            <Route path="/report" element={<Report />} />
+            <Route path="*" element={<NoMatch />} />
           </Route>
         </Routes>
       </Router>
-    </div>
+    </ThemeProvider>
   );
 }
 
